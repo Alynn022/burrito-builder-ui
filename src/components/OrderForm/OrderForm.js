970fe6bc615bab2aafec54ffb9388ac128467611
Lynn Nguyen <AmandaN22@outlook.com>
2022-03-08 10:28:54 -0700

    Add new order to display on app and error handle for submit button

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -10,9 +10,13 @@ class OrderForm extends Component {
     };
   }
 
-
   handleSubmit = e => {
     e.preventDefault();
+    const newOrder = {
+      id: Date.now(),
+      ...this.state
+    }
+    this.props.addOrder(newOrder)
     this.clearInputs();
   }
 
@@ -20,15 +24,22 @@ class OrderForm extends Component {
     this.setState({name: '', ingredients: []});
   }
 
-  handleNameChange = () => {
-    
+  handleNameChange = (e) => {
+    e.preventDefault();
+    this.setState({name: e.target.value})
+  }
+
+  handleIngredientChange = (e) => {
+    e.preventDefault();
+    const ingredient = e.target.name
+   this.setState({ingredients: [...this.state.ingredients, ingredient]})
   }
 
   render() {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
     const ingredientButtons = possibleIngredients.map(ingredient => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button key={ingredient} name={ingredient} onClick={(e) => this.handleIngredientChange(e)}>
           {ingredient}
         </button>
       )
@@ -48,7 +59,7 @@ class OrderForm extends Component {
 
         <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
 
-        <button onClick={e => this.handleSubmit(e)}>
+        <button disabled={!this.state.name || !this.state.ingredients.length} onClick={e => this.handleSubmit(e)}>
           Submit Order
         </button>
       </form>
